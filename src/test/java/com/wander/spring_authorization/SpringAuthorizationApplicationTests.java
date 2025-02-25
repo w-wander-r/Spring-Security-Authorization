9890d9f9af04970d40161af8f948705af518633c
w-wander-r <wanderexe68@gmail.com>
2025-02-25 22:00:48 +0200

    clear project

---
+++ src/test/java/com/wander/spring_authorization/SpringAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.spring_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
